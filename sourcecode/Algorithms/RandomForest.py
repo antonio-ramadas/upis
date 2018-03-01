1357000bf52e680ef37748406d2ae81cb4dbfb4b
António Ramadas <up201303568@gcloud.fe.up.pt>
2018-03-01 15:46:50 +0000

    Enhance Random Forest skeleton

--- sourcecode/Algorithms/RandomForest.py
+++ sourcecode/Algorithms/RandomForest.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 
 from DataProcessor import DataProcessor
+from Headers import SensorProcessedDataHeaders
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-
 
 class RandomForest:
 
-    def __init__(self, data:pd.DataFrame):
-        self.__data__ = data
+    def __init__(self, data: pd.DataFrame):
+        self.__data = data
+        self.__discretize_data()
+
+        self.__rf = RandomForestClassifier()
+
+    def __discretize_data(self):
+        """
+        Removes START and END columns and process them to a new one: DURATION. The values of this column are the
+        discrete conversion of END - START to intervals: short, medium and long.
+
+        This method may be called multiple times that the effect is the same. If the data is already discretisized, then
+        nothing happens.
+        """
+        pass
 
     def fit(self):
-        pass
+        activity_column = SensorProcessedDataHeaders.ACTIVITY
+
+        x = self.__data.drop(columns=activity_column, inplace=True)
+        y = self.__data[activity_column]
+
+        self.__rf.fit(x, y)
 
     def predict(self):
         pass
