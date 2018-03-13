ce81bb976b33fe7ede1f37da920cda56bc95ef4d
António Ramadas <up201303568@gcloud.fe.up.pt>
2018-03-13 17:32:13 +0000

    Add skeleton for metrics of Naive Bayes

--- sourcecode/Algorithms/NaiveBayes.py
+++ sourcecode/Algorithms/NaiveBayes.py
@@ -5,6 +5,7 @@ from DataProcessor import DataProcessor
 from Headers import SensorProcessedDataHeaders, NaiveBayesType
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.naive_bayes import GaussianNB
+from Metrics import *
 import numpy as np
 import pandas as pd
 
@@ -12,7 +13,7 @@ import pandas as pd
 class NaiveBayes:
 
     def __init__(self, dp: DataProcessor, type=NaiveBayesType.SINGLE):
-        self.__data    = dp.data_processed
+        self.__dp      = dp
         self.__type    = type
         # __encoder is currently only being used in MULTIPLE
         self.__encoder = LabelBinarizer()
@@ -32,19 +33,19 @@ class NaiveBayes:
         return np.vstack((x, np.sum(self.__encoder.transform(devices), axis=0)))
 
     # window_size in minutes
-    def __encode_input_of_multiple(self, window_size=15):
+    def __encode_input_of_multiple(self, data, window_size=15):
         col_id = SensorProcessedDataHeaders.ID
         col_start = SensorProcessedDataHeaders.START
 
         # Fit the encoder to all known sensors
-        self.__encoder.fit(self.__data[col_id].unique())
+        self.__encoder.fit(data[col_id].unique())
 
         # x is the training data
-        number_of_devices = self.__data[col_id].unique().size
+        number_of_devices = data[col_id].unique().size
         x = np.empty((0, number_of_devices))
         y = np.empty((0, 1))
 
-        grouped = self.__data.groupby(by=SensorProcessedDataHeaders.ACTIVITY, sort=False)
+        grouped = data.groupby(by=SensorProcessedDataHeaders.ACTIVITY, sort=False)
         window_size *= 60  # convert minutes to seconds
 
         for group_name, group in grouped:
@@ -72,31 +73,59 @@ class NaiveBayes:
 
         return x, y
 
-    def __fit_multiple(self):
-        x, y = self.__encode_input_of_multiple()
+    def __fit_multiple(self, data):
+        self.__number_of_devices = data[SensorProcessedDataHeaders.ID].unique().size
+
+        x, y = self.__encode_input_of_multiple(data)
         self.__nb.fit(x, y)
 
-    def __fit_single(self):
-        x = self.__data[SensorProcessedDataHeaders.ID].values.reshape(-1, 1)
-        y = self.__data[SensorProcessedDataHeaders.ACTIVITY]
+    def __fit_single(self, data):
+        x = data[SensorProcessedDataHeaders.ID].values.reshape(-1, 1)
+        y = data[SensorProcessedDataHeaders.ACTIVITY]
         self.__nb.fit(x, y)
 
-    def fit(self):
+    def fit(self, data):
         if self.__type is NaiveBayesType.SINGLE:
-            self.__fit_single()
+            self.__fit_single(data)
         elif self.__type is NaiveBayesType.MULTIPLE:
-            self.__fit_multiple()
+            self.__fit_multiple(data)
 
     def predict(self, sensor_id):
         if self.__type is NaiveBayesType.MULTIPLE:
-            col_id = SensorProcessedDataHeaders.ID
-
-            number_of_devices = self.__data[col_id].unique().size
-            x = np.empty((0, number_of_devices))
+            # *__number_of_devices* was previously created in *__fit_multiple*
+            x = np.empty((0, self.__number_of_devices))
 
             sensor_id = self.__add_devices_with_encoder(x, sensor_id)
 
         return self.__nb.predict(sensor_id)
+
+    def evaluate(self, n_folds=10):
+        raise NotImplementedError
+
+        activity_column = SensorProcessedDataHeaders.ACTIVITY
+        matrices  = []
+        f1        = 0
+        precision = 0
+        recall    = 0
+
+        for train, test in self.__dp.split(n_folds=n_folds):
+            self.fit(train)
+            # TODO This prediction has to change
+            prediction = self.predict(test)
+
+            metric = Metrics(test[activity_column], prediction)
+
+            f1        += metric.f1()
+            precision += metric.precision()
+            recall    += metric.recall()
+            matrices  += [metric.confusion_matrix()]
+
+        f1        /= n_folds
+        precision /= n_folds
+        recall    /= n_folds
+        matrices   = np.array(matrices)
+
+        return f1, precision, recall, matrices
 
 
 if __name__ == '__main__':
@@ -108,7 +137,7 @@ if __name__ == '__main__':
     dp = DataProcessor(path=path)
 
     nb = NaiveBayes(dp, NaiveBayesType.MULTIPLE)
-    nb.fit()
+    nb.fit(dp.data_processed)
 
     sensor = ['100', '101', '95', '54', '93','72','67','108']
     print('Prediction of the activity when sensor', sensor, 'is active:', nb.predict(sensor))
