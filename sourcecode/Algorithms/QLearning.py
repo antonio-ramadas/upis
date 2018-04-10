e91e507b1fa945a8d84a076570cec476667c029c
António Ramadas <up201303568@gcloud.fe.up.pt>
2018-04-10 10:48:27 +0100

    Move the dataset processor to its own method

--- sourcecode/Algorithms/QLearning.py
+++ sourcecode/Algorithms/QLearning.py
@@ -19,30 +19,43 @@ class QLearning:
         self.__p = p
         self.__n_activities = number_of_activities
         self.__encoder = LabelEncoder()
+        self.n_most_frequent_activities = []
 
-    def algorithm(self):
+    def __process(self, dataset: pd.DataFrame) -> pd.DataFrame:
         # First let's get what we want
         columns = [ActivityDataHeaders.LABEL,
                    ActivityDataHeaders.START_TIME,
                    ActivityDataHeaders.END_TIME]
-        activities_df = p.data()[columns]
+        activities_df = dataset[columns]
 
         label = ActivityDataHeaders.LABEL
 
-        n_most_frequent_activities = activities_df[label].value_counts().index[:self.__n_activities]
+        # Get the most frequent activities
+        self.n_most_frequent_activities = activities_df[label].value_counts().index[:self.__n_activities]
 
-        activities_df = activities_df[activities_df[label].isin(n_most_frequent_activities)]
+        activities_df = activities_df[activities_df[label].isin(self.n_most_frequent_activities)]
 
+        starts = activities_df[[label, ActivityDataHeaders.START_TIME]]
+        ends   = activities_df[[label, ActivityDataHeaders.END_TIME]]
+        ends.rename(index=str, columns={ActivityDataHeaders.END_TIME: ActivityDataHeaders.START_TIME}, inplace=True)
+
+        activities_df = starts.append(ends)
+
+        # By sorting, when an activity first shows, then it starts, if it shows again, then it is its end
+        # We can read now this as a loop
         activities_df.sort_values(ActivityDataHeaders.START_TIME, inplace=True)
 
+        return activities_df
+
+    def algorithm(self):
+        activities_df = self.__process(p.data())
+
         # Build History
-        self.__encoder.fit(n_most_frequent_activities)
+        self.__encoder.fit(self.n_most_frequent_activities)
         initial_state = (False,) * self.__n_activities
         history = {
             initial_state: initial_state
         }
-
-
 
 
 if __name__ == '__main__':
